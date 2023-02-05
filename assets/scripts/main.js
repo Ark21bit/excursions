2e6d4f49b62dc92c058cac1248ae22bee3cbbb38
Ark21bit <webleif@vk.com>
2023-02-05 21:44:24 +0300

    menu

---
+++ assets/scripts/main.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector(".btn-menu");
+let menuBox = document.querySelector(".menu-box");
+let body = document.querySelector("body");
+
+
+
+btn.addEventListener("click", function(event){
+    menuBox.classList.toggle("active");
+    body.classList.toggle("overflow-hidden");
+ });
